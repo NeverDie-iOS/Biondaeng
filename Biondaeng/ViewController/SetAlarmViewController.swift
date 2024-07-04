98610bcac27c4283a6f7220463c8da4ec4dcb2c9
hyunsoo <mm083434@gmail.com>
2024-07-04 14:00:11 +0900

    WIP: Add initial half-modal view functionality
    Basic half-modal view functionality added.
    UI components to be added later.

---
+++ Biondaeng/ViewController/SetAlarmViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class SetAlarmViewController: UIViewController {
+    
+   
+    override func viewDidLoad() {
+       super.viewDidLoad()
+    }
+  
+}
--- Biondaeng/ViewController/WelcomeViewController.swift
+++ Biondaeng/ViewController/WelcomeViewController.swift
@@ -1,9 +1,22 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    }
+    
+    @IBAction func tapStartButton(_ sender: UIButton) {
+        guard let VC = storyboard?.instantiateViewController(identifier: "SetAlarmViewController") as? SetAlarmViewController else {
+                return
+        }
+        
+        VC.modalPresentationStyle = .pageSheet
+        
+        if let sheet = VC.sheetPresentationController {
+            sheet.detents = [.medium(), .large()]
+        }
+        
+        present(VC, animated: true, completion: nil)
     }
 }
